5819e53396b0d12034851ee761766814fc9c0bb4
Muhammad Husnain <aimahusnain@gmail.com>
2023-10-11 04:18:25 -0700

    e

--- components/events/events.js
+++ components/events/events.js
@@ -6,10 +6,9 @@ function Events() {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven&apos;t heard of them.</p>
         </div>
         <div className="flex flex-wrap -m-2">
-          {/* Repeat the following code block for each team member */}
           <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
               <div className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4">
@@ -26,7 +25,6 @@ function Events() {
               </div>
             </div>
           </div>
-          {/* End of code block */}
         </div>
       </div>
     </section>
