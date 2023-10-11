4f1ada99138d9e0fe900aec8f86d9bc19158dd23
Muhammad Husnain <aimahusnain@gmail.com>
2023-10-11 03:44:19 -0700

    Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import Posts from '../components/home/posts';
 
 export default function Home() {
